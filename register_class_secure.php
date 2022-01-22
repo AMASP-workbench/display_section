46a9f30e62a11ce8482d37d72440f039875b38dc
aldus <drp@cms-lab.com>
2022-01-22 22:33:58 +0100

    Update register_class_secure.php

--- register_class_secure.php
+++ register_class_secure.php
@@ -14,7 +14,8 @@
  */
 
 $files_to_register = [
-    "index.php" // only for testing purpose
+    "index.php" ,   // only for testing purpose
+    "precheck.php"  // @notice - 2022-01-22: not realy clear if this one is needed here
 ];
 
 LEPTON_secure::getInstance()->accessFiles( $files_to_register );

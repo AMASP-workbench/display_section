5bef86733bb49692780ec7529bcffff468c86f9c
aldus <drp@cms-lab.com>
2022-05-23 18:05:50 +0200

    Update droplet.php

--- source/droplet.php
+++ source/droplet.php
@@ -3,22 +3,22 @@
 // E.g. for an internal version-string like "0.1.3.23 - 2021-10-03"
 /**
  *
- *  @version 0.2.1.0
+ *  @version 0.2.1.1
  */
 global $database, $TEXT, $oLEPTON, $page_id, $section_id, $HEADERS, $FOOTERS;
 
-// -- 1 get an instance of the class 
+// -- [1] get an instance of the class 
 $oDS = display_section::getInstance();
 
-// -- 2 execute 
+// -- [2] execute 
 $sReturnString = $oDS->getSection( $sid ?? 0, $oLEPTON_page_data );
 
-// -- 3 are there any errors?
+// -- [3] are there any errors?
 if(true === $oDS->isError)
 {
-    // -- 3.1 Format via (TWIG-) template
+    // -- [3.1] Format via (TWIG-) template
     $oDS->formatErrorMessage( $sReturnString );
 }
 
-// -- 4
+// -- [4]
 return $sReturnString;

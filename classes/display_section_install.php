c71f218f4bc0874bdbf5c296c6ae1f5715d481a6
aldus <drp@cms-lab.com>
2022-09-07 07:03:54 +0200

    minor changes and typos

--- classes/display_section_install.php
+++ classes/display_section_install.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
  */
 
 /**
- * Class for install/upgrad module "display_section"
+ * Class for install/upgrade module "display_section"
  *
  * @author  Dietrich Roland Pehlke (Aldus)
  * @package development
@@ -27,19 +27,18 @@ class display_section_install
     const DROPLET_NAME          = "display_section";
     const DROPLET_DESCRIPTION   = "An experimental private work";
     const DROPLET_COMMENT       = "Use: [[display_section?sid=42]]";
-    
+
     const DROPLET_SOURCE_FILE   = "/source/droplet.php";
     
     /**
      *  How many first lines of the php-source of the droplet should be skipped?
      */
     const DROPLETS_SOURCE_SKIP_LINES = 3;
-    
+
     public function __construct()
     {
         // [1] Is the droplet installed?
-        if(!$this->isInstalled())
-        {
+        if (!$this->isInstalled()) {
             // [1.1] Insert an entry in the database
             $this->install();
         } else {
@@ -47,14 +46,14 @@ class display_section_install
             $this->upgrade();
         }
     }
-    
+
     /**
-     *  Uninstall and delete entrie from DB table
+     *  Uninstall and delete entries from DB table
      */
-    static public function uninstall()
+    static public function uninstall(): void
     {
         LEPTON_database::getInstance()->simple_query(
-            "DELETE FROM `".TABLE_PREFIX."mod_droplets` WHERE `name`='".self::DROPLET_NAME."'"
+            "DELETE FROM `" . TABLE_PREFIX . "mod_droplets` WHERE `name`='" . self::DROPLET_NAME . "'"
         );
     }
     
@@ -63,13 +62,13 @@ class display_section_install
      *
      * @return string
      */
-    protected function getDropletSource() : string
+    protected function getDropletSource(): string
     {
         $aTemp = file(
-            dirname(__DIR__).self::DROPLET_SOURCE_FILE,
+            dirname(__DIR__) . self::DROPLET_SOURCE_FILE,
             FILE_IGNORE_NEW_LINES
         );
-        
+
         // Skip first three lines/entries
         return implode("\n", array_slice($aTemp, self::DROPLETS_SOURCE_SKIP_LINES));
     }
@@ -79,58 +78,58 @@ class display_section_install
      *
      * @return bool
      */
-    protected function isInstalled() : bool
+    protected function isInstalled(): bool
     {
         $mixedResult = LEPTON_database::getInstance()->get_one(
-            "SELECT `id` FROM `".TABLE_PREFIX."mod_droplets` WHERE `name`='".self::DROPLET_NAME."'"
+            "SELECT `id` FROM `" . TABLE_PREFIX . "mod_droplets` WHERE `name`='" . self::DROPLET_NAME . "'"
         );
-        
+
         return ($mixedResult !== NULL);
     }
     
     /**
-     * Insert an entry into the currend database.  
+     * Insert an entry into the current database.
      * 
      * @return bool
      */
-    protected function install() : bool
+    protected function install(): bool
     {
         return LEPTON_database::getInstance()->build_and_execute(
             "insert",
-            TABLE_PREFIX."mod_droplets",
+            TABLE_PREFIX . "mod_droplets",
             [
-                "name"          => self::DROPLET_NAME,
-                "description"   => self::DROPLET_DESCRIPTION,
-                "comments"      => self::DROPLET_COMMENT,
-                "code"          => $this->getDropletSource(),
+                "name" => self::DROPLET_NAME,
+                "description" => self::DROPLET_DESCRIPTION,
+                "comments" => self::DROPLET_COMMENT,
+                "code" => $this->getDropletSource(),
                 "modified_when" => time(),
-                "modified_by"   => 1,
-                "active"        => true,
-                "admin_edit"    => 1,
-                "admin_view"    => 1,
-                "show_wysiwyg"  => 1
+                "modified_by" => 1,
+                "active" => true,
+                "admin_edit" => 1,
+                "admin_view" => 1,
+                "show_wysiwyg" => 1
             ]
         );
-        
+
     }
     
     /**
-     * Update an entry inside the currend database.  
+     * Update an entry inside the current database.
      * 
      * @return bool
      */
-    protected function upgrade() : bool
+    protected function upgrade(): bool
     {
         return LEPTON_database::getInstance()->build_and_execute(
             "update",
-            TABLE_PREFIX."mod_droplets",
+            TABLE_PREFIX . "mod_droplets",
             [
-                "description"   => self::DROPLET_DESCRIPTION,
-                "comments"      => self::DROPLET_COMMENT,
-                "code"          => $this->getDropletSource(),
+                "description" => self::DROPLET_DESCRIPTION,
+                "comments" => self::DROPLET_COMMENT,
+                "code" => $this->getDropletSource(),
                 "modified_when" => time()
             ],
-            "`name`='".self::DROPLET_NAME."'"
+            "`name`='" . self::DROPLET_NAME . "'"
         );
     }
 }

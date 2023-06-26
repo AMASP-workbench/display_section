5cfd5635d6e26a52c8485f7fa94cb86acb43aa46
aldus <drp@cms-lab.com>
2023-06-26 20:51:04 +0200

    Update droplet.php

--- source/droplet.php
+++ source/droplet.php
@@ -3,9 +3,9 @@
 // E.g. for an internal version-string like "0.1.3.23 - 2021-10-03"
 /**
  *
- *  @version 0.2.1.2
+ *  @version 0.2.1.3
  */
-global $database, $TEXT, $oLEPTON, $page_id, $section_id, $HEADERS, $FOOTERS, $oLEPTON_page_data;
+global $database, $TEXT, $oLEPTON, $page_id, $section_id, $HEADERS, $FOOTERS;
 
 // -- [1] get an instance of the class
 $oDS = display_section::getInstance();
